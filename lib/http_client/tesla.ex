a077e0c1099cd3991dfe854ae3381e9d43bdde47
murilobauerc <murilo.bauer@sumup.com>
2022-01-22 20:01:08 -0300

    exposed all_todos and get_todo_by_id from http client tesla

--- lib/http_client/tesla.ex
+++ lib/http_client/tesla.ex
@@ -4,13 +4,7 @@ defmodule HttpClient.Tesla do
 
   plug Tesla.Middleware.BaseUrl, @base_url
 
-  def fetch_all_completed_todos() do
-    fetch_data()
-    |> parse_body()
-    |> get_completed_todos()
-  end
-
-  def fetch_data() do
+  def all_todos() do
     case get("/todos") do
       {:ok, %Tesla.Env{status: 200, body: body}} -> body
       {:ok, %Tesla.Env{status: 404}} -> {:error, "URL not found. 404"}
@@ -18,11 +12,41 @@ defmodule HttpClient.Tesla do
     end
   end
 
+  def todo_id(id) do
+    case get("/todos/#{id}") do
+      {:ok, %Tesla.Env{status: 200, body: body}} -> body
+      {:ok, %Tesla.Env{status: 404}} -> {:error, "URL not found. 404"}
+    end
+  end
+
+  @doc """
+    Returns parsed detailed todo given a todo_id.
+  """
+  def get_by_todo_id(id) do
+    id
+    |> todo_id()
+    |> parse_body()
+    |> filter_todo_id()
+  end
+
+  @doc """
+    Returns a parsed detailed list of all todos completed.
+  """
+  def list_all_by_completed_todos() do
+    all_todos()
+    |> parse_body()
+    |> filter_completed_todos()
+  end
+
   def parse_body(input) do
     Jason.decode(input)
   end
 
-  def get_completed_todos({:ok, todos}) do
+  def filter_todo_id({:ok, todo}) do
+    todo
+  end
+
+  def filter_completed_todos({:ok, todos}) do
     todos
     |> Enum.filter(&(Map.get(&1, "completed")))
   end
